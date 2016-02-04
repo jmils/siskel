e0ea5769a1131239faf4d0c2063f80e283d647a4
jmils <jmils66@gmail.com>
2016-02-04 16:44:39 -0500

    Add Siskel group coding folder to repo

--- siskel.rb
+++ siskel.rb
@@ -1,10 +1,33 @@
 require 'httparty'
 
 class Siskel
-  attr_reader :title
+  attr_reader :title , :rating, :year, :plot
 
-  def initialize(title)
-    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}")
+  def initialize(title, options = {})
+    year = options[:year]
+    x = options[:plot]
+    @movie = HTTParty.get("http://www.omdbapi.com/?t=#{title}&y=#{year}&plot=#{x}&tomatoes=true")
+    @tomato = @movie['tomatoMeter'].to_i
     @title = @movie['Title']
+    @rating = @movie['Rated']
+    @year = @movie['Year']
+    @plot = @movie['Plot']
   end
+
+  def title
+    if @title == nil
+      @title = "Movie not found!"
+    else
+      @title = @movie['Title']
+    end
+  end
+
+  def consensus
+    if @tomato >= 51
+       "Thumbs Up"
+     else
+       "Thumbs Down"
+     end
+  end
+#ends class
 end
